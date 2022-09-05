6b5ad87a2ced7e4f4d8c6dea106a6ebeab1218cf
Денис Сиразов <denis@MacBook-Air-Denis.local>
2022-09-05 19:47:58 +0300

    stated cpp04

--- cpp04/ex00/Animal.hpp
+++ cpp04/ex00/Animal.hpp
@@ -12,26 +12,8 @@ class Animal
         ~Animal();
         Animal(const Animal &src);
         Animal &operator=(const Animal &arg);    
-};
-
-class Dog : public Animal
-{
-    public:
-        Dog();
-        ~Dog();
-        Dog(const Dog &src);
-        Dog &operator=(const Dog &arg);   
-
-};
-
-class Cat : public Animal
-{
-    public:
-        Cat();
-        ~Cat();
-        Cat(const Cat &src);
-        Cat &operator=(const Cat &arg);   
-
+        std::string getTyoe() const;
+        virtual void makeSound() const;
 };
 
 #endif
---
+++ cpp04/ex00/WrongAnimal.hpp
@@ -0,0 +1,17 @@
+#ifndef WRONGANIMAL_HPP
+#define WRONGANIMAL_HPP
+
+#include <iostream>
+
+class WrongAnimal
+{
+    public:
+        WrongAnimal();
+        ~WrongAnimal();
+        WrongAnimal(const WrongAnimal &src);
+        WrongAnimal &operator=(const WrongAnimal &arg);
+        std::string getType() const;
+        void makeSound() const;
+};
+
+#endif
