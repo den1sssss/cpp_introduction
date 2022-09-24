2e62c5e21394e9ed9bc677ca124594212de430b5
Денис Сиразов <denis@MacBook-Air-Denis.local>
2022-09-24 22:31:01 +0300

    ex00 done

--- cpp05/ex00/Bureaucrat.cpp
+++ cpp05/ex00/Bureaucrat.cpp
@@ -17,28 +17,31 @@ Bureaucrat& Bureaucrat::operator=(Bureaucrat const &src)
     grade = src.getGrade();
     return (*this);
 }
-Bureaucrat::Bureaucrat(const std::string name, int grade)
+Bureaucrat::Bureaucrat(const std::string name, int grade):name(name),grade(grade)
 {
-
+    if(getGrade() < 1)
+        throw Bureaucrat::GradeTooHighException();
+    if(getGrade() > 150)
+        throw Bureaucrat::GradeTooLowException();
 }
 std::string const Bureaucrat::getName() const
 {
-
+    return name;
 }
 
 int Bureaucrat::getGrade() const
 {
-
+    return grade;
 }
 
 void Bureaucrat::increase()
 {
-
+    grade++;
 }
 
 void Bureaucrat::decrease()
 {
-
+    grade--;
 }
 
 const char *Bureaucrat::GradeTooHighException::what() const throw()
@@ -51,5 +54,6 @@ const char *Bureaucrat::GradeTooLowException::what() const throw()
 }
 std::ostream &operator<<(std::ostream &cout, Bureaucrat const &src)
 {
-    
+    cout << src.getName() << ", bureaucrat grade " << src.getGrade() << std::endl;   
+    return cout;
 }
--- cpp05/ex00/main.cpp
+++ cpp05/ex00/main.cpp
@@ -4,11 +4,26 @@ int main()
 {
     try
     {
-        /* code */
+        Bureaucrat bur1("Denis",1);
+        Bureaucrat bur2("Andrew", 50);
+
+        bur1.decrease();
+        std::cout << bur1 << std::endl;
+
+        bur2.increase();
+        std::cout << bur2 << std::endl;
+
+
+        Bureaucrat bur3("Aboba",0);
+        std::cout << bur3 << std::endl;
+
+        Bureaucrat bur4("Nedoidet",12);
+        std::cout << bur4 << std::endl;
+
     }
     catch(const std::exception& e)
     {
-        std::cerr << e.what() << '\n';
+        std::cerr << e.what() << std::endl;
     }
        
 }
