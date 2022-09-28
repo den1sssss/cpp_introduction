ed88f5903b5d45147a3ac23762df6f8e33bde5ba
Денис Сиразов <denis@MacBook-Air-Denis.local>
2022-09-29 00:19:05 +0300

    half of ex01

--- cpp07/ex01/iter.hpp
+++ cpp07/ex01/iter.hpp
@@ -1,16 +1,14 @@
 #ifndef ITER_HPP
 # define ITER_HPP
+#include <iostream>
+#include <string.h>
 
 template<typename T>
-void swap(T &a, T &b)
+void iter(T *str, int size, void func(T &))
 {
-    T temp;
-
-    temp = a;
-    a = b;
-    b = temp;
+    int i = 0;
+    while(i<size)
+        func(str[i++]);
 }
-
-
 
 #endif
--- cpp07/ex01/main.cpp
+++ cpp07/ex01/main.cpp
@@ -1,8 +1,13 @@
 #include "iter.hpp"
 
-
+void toUpper(char &c)
+{
+    // return(c+32);
+    std::cout<< char(c -32);
+}
 
 int main(void)
 {
-    
+    char str[]={'a','b','o','b','a'};
+    ::iter(str,5,toUpper);
 }
